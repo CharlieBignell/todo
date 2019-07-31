74d5789776f04e5dcf724880c651f6c248a3110e
Charlie Bignell <charlie_jb@hotmail.co.uk>
2019-07-31 21:28:30 +0100

    Added ability to add tasks

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ let app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'pug')
 
-app.get('/', function (req, res) {
-    res.render('index')
-})
+let task = ["buy socks", "practise with nodejs"]
 
 app.post('/addtask', function (req, res) {
-    res.render('index')
-})
+    let newTask = req.body.newtask
+    task.push(newTask)
+    res.redirect("/")
+});
+
+app.get("/", function (req, res) {
+    res.render("index", { task: task })
+});
 
 app.listen(3000, function () {
     console.log('Listening on port 3000')
