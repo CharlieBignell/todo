856873cf2d01556d1907ba09b08bf75abff868ce
Charlie Bignell <charlie_jb@hotmail.co.uk>
2019-08-01 16:52:49 +0100

    Added ability to complete tasks

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 let app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 app.set('view engine', 'pug')
 
 app.use(express.static(__dirname + '/public'));
@@ -14,29 +16,20 @@ let completed = ["finished task"]
 // POST methods
 app.post('/addtask', function (req, res) {
     let newTask = req.body.newtask
-    if(newTask.toString().length > 0){tasks.push(newTask)}
+    if (newTask.toString().length > 0) { tasks.push(newTask) }
     res.redirect("/")
 });
 
-app.post("/removetask", function (req, res) {
-    let completeTask = req.body.check;
-    console.log(completeTask)
-    if (typeof completeTask === "string") {
-        complete.push(completeTask);
-        tasks.splice(tasks.indexOf(completeTask), 1);
-    } else if (typeof completeTask === "object") {
-        for (var i = 0; i < completeTask.length; i++) {
-            complete.push(completeTask[i]);
-            tasks.splice(tasks.indexOf(completeTask[i]), 1);
-        }
-    }
+app.post("/completeTask", function (req, res) {
+    let task = req.body.task
+    completed.push(task);
+    tasks.splice(tasks.indexOf(task), 1);
     res.redirect("/");
 });
 
 
 // GET methods
 app.get("/", function (req, res) {
-    console.log(typeof tasks)
     res.render("index", { tasks: tasks, completed: completed })
 });
 
---
+++ public/js/main.js
@@ -0,0 +1,39 @@
+// Get the two list elements
+taskList_active = document.getElementById('taskList_active')
+taskList_completed = document.getElementById('taskList_completed')
+
+
+// For each active task
+tasks_active.forEach(function (task) {
+    let task_el = document.createElement('div')
+    task_el.classList.add('task')
+
+    // The task text
+    let p = document.createElement('p')
+    p.innerHTML = task
+
+    // The 'remove' button
+    let b = document.createElement('button')
+    b.addEventListener('click', function (e) {
+
+        // Send an XMLHTTP POST request with the task to remove
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", '/completeTask', true)
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.send(JSON.stringify({ task: task }))
+
+        location.reload()
+    })
+    b.innerHTML = "Done"
+
+    task_el.appendChild(p)
+    task_el.appendChild(b)
+    taskList_active.appendChild(task_el)
+})
+
+// For each of the completed tasks
+tasks_complete.forEach(function (task) {
+    let task_el = document.createElement('p')
+    task_el.innerHTML = task
+    taskList_completed.appendChild(task_el)
+})
