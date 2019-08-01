1d05b5e73d3974f0fb520328afd5f6d6d34208a1
Charlie Bignell <charlie_jb@hotmail.co.uk>
2019-08-01 14:35:50 +0100

    Refactored 'add task' functionality

--- index.js
+++ index.js
@@ -6,18 +6,42 @@ let app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'pug')
 
-let task = ["buy socks", "practise with nodejs"]
+app.use(express.static(__dirname + '/public'));
 
+let tasks = ["active task"]
+let completed = ["finished task"]
+
+// POST methods
 app.post('/addtask', function (req, res) {
     let newTask = req.body.newtask
-    task.push(newTask)
+    if(newTask.toString().length > 0){tasks.push(newTask)}
     res.redirect("/")
 });
 
-app.get("/", function (req, res) {
-    res.render("index", { task: task })
+app.post("/removetask", function (req, res) {
+    let completeTask = req.body.check;
+    console.log(completeTask)
+    if (typeof completeTask === "string") {
+        complete.push(completeTask);
+        tasks.splice(tasks.indexOf(completeTask), 1);
+    } else if (typeof completeTask === "object") {
+        for (var i = 0; i < completeTask.length; i++) {
+            complete.push(completeTask[i]);
+            tasks.splice(tasks.indexOf(completeTask[i]), 1);
+        }
+    }
+    res.redirect("/");
 });
 
+
+// GET methods
+app.get("/", function (req, res) {
+    console.log(typeof tasks)
+    res.render("index", { tasks: tasks, completed: completed })
+});
+
+
+// Start server
 app.listen(3000, function () {
     console.log('Listening on port 3000')
 })
